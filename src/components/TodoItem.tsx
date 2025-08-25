50b7e41852e01270f23e990698c23b0b07e20c64
johanperjulius1 <johan.per.julius.berglund@gmail.com>
2025-08-25 16:00:58 +0200

    adding the text input field in todo and corresponding event handlers

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,15 +1,22 @@
 interface TodoItemProps {
     isCompleted: boolean;
     onToggle: () => void;
-}
+    userInput: string;
+    onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;}
 
-export default function TodoItem({ isCompleted, onToggle }: TodoItemProps) {
+export default function TodoItem({ isCompleted, onToggle, userInput, onChange }: TodoItemProps) {
     return (
         <div>
             <input
                 type="checkbox"
                 checked={isCompleted}
                 onChange={onToggle}
+            />
+            <input
+                type="text"
+                placeholder="Enter todo..."
+                onChange={onChange}
+                value={userInput}
             />
         </div>
     )
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -3,14 +3,25 @@ import TodoItem from "./TodoItem"
 
 export default function TodoList() {
     const [isCompleted, setIsCompleted] = useState(false)
+    const [userInput, setUserInput] = useState("")
 
     function handleCompleted() {
         setIsCompleted(isCompleted => !isCompleted)
     }
 
+    function handleUserInput(event: React.ChangeEvent<HTMLInputElement>) {
+        setUserInput(event.target.value)
+    }
+
+
     return (
         <section>
-            <TodoItem onToggle={handleCompleted} isCompleted={isCompleted} />
+            <TodoItem
+                onToggle={handleCompleted}
+                isCompleted={isCompleted}
+                userInput={userInput}
+                onChange={handleUserInput}
+            />
             <p>first item</p>
             <p>second item</p>
         </section>
